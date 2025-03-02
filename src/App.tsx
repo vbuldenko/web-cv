ddc428539288d1d72c2b9e1b8721989865cbbe42
Vladyslav <vladbuldenko@gmail.com>
2025-03-02 02:23:42 +0200

    App update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Portfolio from "./components/portfolio/Portfolio";
 import Qualification from "./components/qualification/Qualification";
+import ScrollUp from "./components/scrollup/ScrollUp";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
-import Testimonials from "./components/testimonials/Testimonials";
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <main className="main">
         <Home />
@@ -19,11 +20,10 @@ export default function App() {
         <Services />
         <Qualification />
         <Portfolio />
-        {/* <Testimonials /> */}
         <Contact />
       </main>
-      {/* <Footer />
-      <ScrollUp /> */}
-    </div>
+      <Footer />
+      <ScrollUp />
+    </>
   );
 }
